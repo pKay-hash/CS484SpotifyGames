402726d34ea5d71374a60ecbfa9ab34b446c5ca3
smehdi29 <syed.business29@gmail.com>
2024-09-14 23:36:23 -0500

    style: add pictures of "big 3" and new styling for login in progress

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,6 +4,10 @@ import {React, useState} from 'react';
 import logo from '../assets/logo.png'; // Import the logo (nice)
 import styles from './Login.module.css';
 import Header from './Header';
+import drake from '../assets/drake.png';
+import cole from '../assets/coleworld.png';
+import kdot from '../assets/Kendrick-Lamar.png';
+
 
 const clientId = import.meta.env.VITE_CLIENT_ID || '8d028dc7fb1741a5affeae86686969fe';
 const redirectUri = import.meta.env.VITE_REDIRECT_URI || 'http://localhost:5173/';
@@ -20,31 +24,47 @@ const Login = () => {
     <div className={`min-h-screen flex flex-col content-center bg-gradient-to-r from-stone-900 to-zinc-800`}>
       {/* <img src={logo} alt="Games On The Spot Logo" className="w-8 h-8 ml-20 justify-self-start" /> */}
       <Header />
-      <h1 className="font-teko font-semibold text-8xl mt-7 mb-4 text-center text-green-500">Welcome to </h1>
-      <h1 className="font-teko font-semibold text-8xl mb-6 text-center bg-gradient-to-r from-green-400 from-20% via-green-200 via-30% to-emerald-500 to-90% inline-block text-transparent bg-clip-text">Games On The Spot!</h1>
-      <p className="font-oswald text-lg mb-8 text-center text-gray-300">Discover your music taste through fun, interactive games that are curated from your Spotify Premium Account without their permission!</p>
-      <div className="flex flex-row justify-center content-center space-x-8">
+      <div className="content flex flex-row">
+        <div className="title flex flex-col bg-zinc-800 border-zinc-800 border-r-2 border-b-2 rounded-br-3xl justify-start mr-[25em] px-10 py-10 ">
+          <h1 className="font-teko font-semibold text-8xl mt-7 mb-4 text-start text-green-500">Welcome to </h1>
+          <h1 className="font-teko font-semibold text-8xl mb-6 text-start bg-gradient-to-r from-zinc-400 to-zinc-50 bg-clip-text text-transparent inline-block text-transparent bg-clip-text">Games On The Spot!</h1>
+          <p className="font-oswald text-lg mb-8 text-start text-gray-300">Discover your music taste through fun, interactive games that are curated from your Spotify Premium Account without their permission!</p>
+          <div className="flex flex-row justify-center content-center space-x-8">
 
-        <div className="w-96 bg-zinc-800 bg-opacity-80 rounded-lg shadow-xl overflow-hidden">
-          <div className="p-8">
-            
-            <div className="space-y-4">
-              <p className="text-sm text-gray-400 text-center">Connect with your Spotify account to start playing</p>
-              <a 
-                href={AUTH_URL}
-                className="block w-full py-3 px-4 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-full text-center focus:outline-none animate-pulsate focus:ring-2 focus:ring-green-400 focus:ring-opacity-50"
-              >
-                Login with Spotify
-              </a>
+          <div className="w-96 bg-zinc-700 bg-opacity-80 rounded-lg shadow-xl overflow-hidden mr-10">
+            <div className="p-8">
+              
+              <div className="space-y-4">
+                {/* <p className="text-sm text-gray-400 text-center">Connect with your Spotify account to start playing</p> */}
+                <a 
+                  href={AUTH_URL}
+                  className="block w-full flex flex-row content-center justify-center py-3 px-4 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-full text-center focus:outline-none animate-pulsate focus:ring-2 focus:ring-green-400 focus:ring-opacity-50"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-2">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
+                  </svg>
+                  Login with Spotify
+                </a>
+              </div>
+            </div>
+            <div className="px-8 py-4 bg-zinc-900 bg-opacity-80 border-t border-gray-800">
+              <p className="text-xs text-gray-500 text-center">
+                By logging in, you agree to our Terms of Service and Privacy Policy.
+              </p>
             </div>
           </div>
-          <div className="px-8 py-4 bg-zinc-900 bg-opacity-80 border-t border-gray-800">
-            <p className="text-xs text-gray-500 text-center">
-              By logging in, you agree to our Terms of Service and Privacy Policy.
-            </p>
-          </div>
+          
+        </div>
+        </div>
+        <div className="pictures ml-[-20em]">
+          <img src={cole} className='w-72 h-72 absolute top-10 left-190 transform origin-top-left translate-x-60 translate-y-0  opacity-80 animate-animateCole'></img>
+          <img src={kdot} className='w-84 h-72 absolute top-32 left-160 transform origin-top-left translate-x-20 translate-y-10  opacity-80 animate-animateKdot'></img>
+          <img src={drake} className='w-84 h-84 absolute top-60 left-120 transform origin-top-left translate-x-[-10] translate-y-40  opacity-80 animate-animateDrake'></img>
         </div>
       </div>
+      
+      
+      
       
       <div className="absolute bottom-4 text-center w-full">
         <p className="text-xs text-gray-500 max-w-md mx-auto">
@@ -55,6 +75,7 @@ const Login = () => {
         </p>
       </div>
     </div>
+    
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
   ],
   theme: {
     extend: {
-      fontFamily:{teko:["Teko", 'sans-serif'],
+      fontFamily:{
+        teko:["Teko", 'sans-serif'],
         oswald:["Oswald", 'sans-serif']
       },
       keyframes: {
@@ -14,9 +15,16 @@ module.exports = {
           '0%, 100%': { transform: 'scale(1)' },
           '50%': { transform: 'scale(1.05)' },
         },
+        animateImage:{
+          '0%, 100%': {transform: 'scale(1)', opacity: '0.8'},
+          '50%': { transform: 'scale(1.05)', opacity: '1.0' },
+        },
       },
       animation: {
         pulsate: 'pulsate 1s ease-in-out infinite',
+        animateDrake: 'animateImage 6s ease-in-out infinite 0s',
+        animateKdot: 'animateImage 6s ease-in-out infinite 2s',
+        animateCole: 'animateImage 6s ease-in-out infinite 4s',
       },
     },
   },
