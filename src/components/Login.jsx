971e4d302e2c9a2c6bf44f2326f1e13265ad523e
smehdi29 <syed.business29@gmail.com>
2024-11-07 15:55:30 -0600

    landing page done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -59,8 +59,8 @@ const LoginBox = () => {
 
 const InfoBox = ({header, body}) => {
   return(
-    <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-zinc-900 dark:border-gray-700 w-72 h-40 transform transition-transform duration-300 hover:scale-105">
-      <div className='font-teko text-slate-50 text-2xl text-center p-4'>
+    <div className="bg-white border border-gray-200 rounded-lg shadow dark:bg-zinc-900 dark:border-gray-700 w-[20em] h-[12em] transform transition-transform duration-300 hover:scale-105">
+      <div className='font-teko font-semibold text-slate-50 text-3xl text-center p-4'>
         {header}
       </div>
       <div className='font-oswald text-slate-400 text-sm p-4'>
@@ -75,10 +75,10 @@ const Login = () => {
 
   const handleOpen = () => setGetStarted(!getStarted);
   return (
-    <div className={`min-h-screen  flex flex-col content-center bg-gradient-to-r from-stone-900 to-zinc-800`}>
-      <Header />
-      <div className="content flex flex-row">
-        <div className="title flex flex-col bg-zinc-800 border-zinc-800 border-r-2 border-b-2 rounded-br-3xl mr-[25em] px-10 py-10">
+    <div className={`landing min-h-screen max-w-screen flex flex-col content-center bg-gradient-to-r from-stone-900 to-zinc-800`}>
+      {/* <Header /> */}
+      <div className="content justify-center items-center flex flex-row">
+        <div className="title flex flex-col justify-center items-center bg-zinc-800 border-zinc-800 border-r-2 border-b-2 rounded-bl-3xl rounded-br-3xl px-10 py-10">
           <h1 className="font-teko font-semibold text-8xl mt-7 mb-4 text-start text-green-500">Welcome to </h1>
           <h1 className="font-teko font-semibold text-8xl mb-6 text-start bg-gradient-to-r from-zinc-400 to-zinc-50 bg-clip-text text-transparent inline-block text-transparent bg-clip-text">Games On The Spot!</h1>
           <p className="font-oswald text-lg mb-8 text-start text-gray-300">Discover your music taste through fun, interactive games that are curated from your Spotify Premium Account!</p>
@@ -88,9 +88,9 @@ const Login = () => {
                 <div className = "content flex flex-row space-x-4">
                  <button  onClick={handleOpen} className='block w-full flex flex-row items-center content-center justify-center py-3 px-4 bg-green-500 hover:bg-green-600 text-white font-semibold rounded-full text-center focus:outline-none '> 
                                 Get Started </button>
-                  <a href={github_url} className='block w-full flex flex-row items-center content-center justify-center py-3 px-4 bg-slate-400 hover:bg-slate-600 text-white font-semibold rounded-full text-center focus:outline-none '> 
+                  {/* <a href={github_url} className='block w-full flex flex-row items-center content-center justify-center py-3 px-4 bg-slate-400 hover:bg-slate-600 text-white font-semibold rounded-full text-center focus:outline-none '> 
                     <img src={gh} className='w-6 h-6 mr-1 '></img>
-                    View GitHub </a> 
+                    View GitHub </a>  */}
                 </div>
                 )}
                   {getStarted && 
@@ -99,9 +99,9 @@ const Login = () => {
             </div>
         </div>
         </div>
-        <div className="cole mt-[6em]">
+        {/* <div className="cole mt-[6em]">
           <iframe src="https://giphy.com/embed/K6359BjXyiAFi" width="500" height="350" className="giphy-embed" allowFullScreen></iframe>
-        </div>
+        </div> */}
         {/*IMAGES of big 3 <div className="pictures ml-[-20em]">
           <img src={cole} className='w-72 h-72 absolute top-10 left-190 transform origin-top-left translate-x-60 translate-y-0  opacity-80 animate-animateCole'></img>
           <img src={kdot} className='w-84 h-72 absolute top-32 left-160 transform origin-top-left translate-x-20 translate-y-10  opacity-80 animate-animateKdot'></img>
